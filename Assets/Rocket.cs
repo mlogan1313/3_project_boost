1022cc2dfa3490374b82b5a5d6b668aa7db39949
Logan, Matt (Verity Services LLC) <matt.logan@contractor.p66.com>
2018-04-23 07:58:55 -0500

    added thrusting with sound

---
+++ Assets/Rocket.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    private AudioSource _rocketSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rocketSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidBody.AddRelativeForce(Vector3.up);
+            if (!_rocketSound.isPlaying) _rocketSound.Play();
+        }
+        else
+        {
+            _rocketSound.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
